af69930d19d23719d8eff45bf38cb3b989742c68
Estelle Martinez <rikku.shimizu@gmail.com>
2016-07-30 12:41:23 +0200

    Ahoy

---
+++ Assets/Scripts/BaballLauncher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaballLauncher : MonoBehaviour {
+
+    public Transform transform;
+    public Transform otherTransform;
+    public GameObject baballPrefab;
+
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown(0))
+        {
+            Instantiate(baballPrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+
+}
