0b224df6abd068a27e0f295b46e699b2d7a4e1e8
Estelle Martinez <rikku.shimizu@gmail.com>
2016-07-30 18:30:38 +0200

    Meow

---
+++ Assets/Scripts/PurrateSkinManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PurrateSkinManager : MonoBehaviour
+{
+    [System.Serializable]
+    public enum SkinColor
+    {
+        blue,
+        green,
+        pink,
+        yellow
+    }
+
+    [SerializeField]
+    private SkinColor skinColor = SkinColor.yellow;
+    /*{
+        get
+        {
+            return color;
+        }
+        set
+        {
+            color = value;
+            UpdateColor();
+        }
+    }*/
+
+    private SkinColor color;
+
+    private void UpdateColor()
+    {
+        Debug.Log("UPDATE COLOR");
+    }
+}
