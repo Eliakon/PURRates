27c82dc3c75104562041e18564bd6e7f4f696a15
David Corticchiato <david.corticchiato@gmail.com>
2016-08-13 16:59:46 +0200

    first attempt on island enlights

--- Assets/Scripts/Island.cs
+++ Assets/Scripts/Island.cs
@@ -5,8 +5,43 @@ using System.Collections.Generic;
 public class Island : MonoBehaviour
 {
     public List<IslandPart> parts;
+    public Material enlightedMaterial;
+    public Material normalMaterial;
 
-    public void OnPointerEnterPart(IslandPart part) {}
-    public void OnPointerExitPart(IslandPart part) {}
-    public void OnClickPart(IslandPart part) {}
+    private bool isEnlighted = false;
+    private int shouldResetEnlight = 0;
+
+
+    public void OnPointerEnterPart(IslandPart sourcePart) {
+        if (isEnlighted) return;
+
+        isEnlighted = true;
+        shouldResetEnlight = 0;
+
+        foreach (IslandPart part in parts) {
+            part.ChangeMaterial(enlightedMaterial);
+        }
+    }
+
+    public void OnPointerExitPart(IslandPart sourcePart) {
+        if (isEnlighted) shouldResetEnlight = 5;
+    }
+
+    public void OnClickPart(IslandPart sourcePart) {
+
+    }
+
+    void Update() {
+        if (isEnlighted && shouldResetEnlight > 0) {
+            shouldResetEnlight--;
+
+            if (shouldResetEnlight == 0) {
+                foreach (IslandPart part in parts) {
+                    part.ChangeMaterial(normalMaterial);
+                }
+
+                isEnlighted = false;
+            }
+        }
+    }
 }
--- Assets/Scripts/IslandPart.cs
+++ Assets/Scripts/IslandPart.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,6 +7,11 @@ public class IslandPart : MonoBehaviour
 {
 
   public Island island;
+  public Image image;
+
+  public void ChangeMaterial(Material material) {
+    image.material = material;
+  }
 
   public void OnPointerEnter() {
     island.OnPointerEnterPart(this);
